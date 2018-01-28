e914c18b7a96096ba0527830d6fc0da64edef332
Dries De Rudder <info@drudr.be>
2018-01-28 13:18:00 +0100

    fix Server-type import

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 
-/// <reference types="restify" />
 /// <reference types="aws-lambda" />
+
+import { Server } from "restify";
 
 export function serverless(app: Server): (event: AWSLambda.APIGatewayEvent, context: AWSLambda.Context, callback: AWSLambda.ProxyCallback) => void;
